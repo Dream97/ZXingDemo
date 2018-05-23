ed0994cbb142f78eafca42addf42369881ce3dc7
Dream97 <1248310717@qq.com>
2018-05-24 01:05:44 +0800

    上传资源文件

---
+++ zxing/Constants.java
@@ -0,0 +1,10 @@
+package com.nongyan.teastore.utils;
+
+public class Constants {
+  
+    // request参数
+    public static final int REQ_QR_CODE = 11002; // // 打开扫描界面请求码
+    public static final int REQ_PERM_CAMERA = 11003; // 打开摄像头
+
+    public static final String INTENT_EXTRA_KEY_QR_SCAN = "qr_scan_result";
+}
